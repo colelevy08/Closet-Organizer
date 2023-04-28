541b5a0d1dcddc83a123a520207f9d8824e1510d
colelevy <colelevy08@gmail.com>
2023-04-28 15:36:37 -0400

    final

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './Header';
 import ClosetContainer from './ClosetContainer';
 import AddedClothes from './AddedClothes';
+import ClothingInspiration from './ClothingInspiration'; // Import your new component
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -15,10 +16,13 @@ function App() {
           <Link to="/">Home</Link>
           {" | "}
           <Link to="/added-clothes">View Added Clothes</Link>
+          {" | "}
+          <Link to="/inspiration">Clothing Inspiration</Link> {/* Add a link to your new page */}
         </nav>
         <Routes>
           <Route path="/" element={<ClosetContainer />} />
           <Route path="/added-clothes" element={<AddedClothes />} />
+          <Route path="/inspiration" element={<ClothingInspiration />} /> {/* Add a route for your new page */}
         </Routes>
       </div>
     </Router>
---
+++ my-app/src/components/ClothingInspiration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ClothingInspiration() {
+  return (
+    <div>
+      <h2>Clothing Inspiration</h2>
+      {/* Your content goes here. */}
+    </div>
+  );
+}
+
+export default ClothingInspiration;
